e26e8225242c42c44aafa5f2b5dc9c4880fd0ef2
LeaBani <leabani92@gmail.com>
2023-02-20 13:44:31 +0100

    add eslint

--- fitwork/src/App.js
+++ fitwork/src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1>FitWork</h1>
-  
-  <Button as="a" variant="primary">
-    Button as link
-  </Button>
-  <Button as="a" variant="success">
-    Button as link
-  </Button>
+
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+      <Button as="a" variant="success">
+        Button as link
+      </Button>
 
     </div>
   );
