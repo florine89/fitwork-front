92620a0d45b84a11357c4dbf03161b03fb493ef7
Adnane <adnane.lahcene@icloud.com>
2023-02-20 14:40:36 +0100

    add folders

---
+++ fitwork/src/stores/index.js
@@ -0,0 +1,14 @@
+// import { createStore, compose } from 'redux';
+
+// // import reducer from 'src/reducers';
+// // import recipesAPI from 'src/middlewares/recipesAPI';
+
+// const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+// // const enhancers = composeEnhancers(
+// //   applyMiddleware(recipesAPI),
+// // );
+
+// const store = createStore(reducer, enhancers);
+
+// export default store;
