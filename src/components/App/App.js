2cba4965eff290e8b45336124c7079c5d481b145
florine89 <ferreiraflorine@gmail.com>
2023-02-21 13:45:37 +0100

    Routes en dur

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,25 @@ import { Route, Routes } from 'react-router-dom';
 import Inscription from '../Inscription/Inscription';
 import LoginForm from '../LoginForm';
 
-
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-    <Routes>
-      <h1>FitWork</h1>
-      <Route path="/inscription" element={<Inscription />} />
-      <Route path="/connection" element={<LoginForm />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/inscription" element={<Inscription />} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/profil" element={<h1>Profil</h1>} />
+        <Route path="/categorie" element={<h1>Catégorie</h1>} />
+        <Route path="/categorie/:id" element={<h1>Articles</h1>} />
+        <Route path="/programme" element={<h1>Programme</h1>} />
+        <Route path="/favoris" element={<h1>Element</h1>} />
+        <Route path="/administrateur" element={<h1>Administrateur</h1>} />
+        <Route path="*" element={<h1>Page d'erreur</h1>} />
+      </Routes>
     </div>
- );
+  );
 }
 
 export default App;
