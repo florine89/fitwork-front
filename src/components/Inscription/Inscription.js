0b8b475cfd2c0c354b95bdcc9ed8140b81d59788
Adnane <adnane.lahcene@icloud.com>
2023-02-21 11:07:49 +0100

    modification of the form with bootstrap

--- src/components/Inscription/Inscription.js
+++ src/components/Inscription/Inscription.js
@@ -1,20 +1,40 @@
-import Birthday from './Birthday/Birthday';
-import InputEmail from './InputEmail/InputEmail';
-import InputFirstName from './InputFirstName/InputFirstName';
-import InputLastName from './InputLastName/InputLastName';
-import Sexe from './Sexe/Sexe';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './Style.scss';
-import Submit from './Submit/Submit';
 
 export default function Inscription() {
   return (
-    <form>
-      <InputLastName />
-      <InputFirstName />
-      <InputEmail />
-      <Birthday />
-      <Sexe />
-      <Submit />
-    </form>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Nom :</Form.Label>
+        <Form.Control type="text" placeholder="Entrez votre nom" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Prénom :</Form.Label>
+        <Form.Control type="text" placeholder="Entrez votre prenom" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email :</Form.Label>
+        <Form.Control type="email" placeholder="Entrez votre email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Mot de passe :</Form.Label>
+        <Form.Control type="password" placeholder="Mot de passe" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Date de naissance :</Form.Label>
+        <Form.Control type="date" placeholder="Date de naissance" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Masculin" checked />
+        <Form.Check type="checkbox" label="Féminin" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Enregistrer
+      </Button>
+      <Button variant="primary" type="reset">
+        Annuler
+      </Button>
+    </Form>
   );
 }
