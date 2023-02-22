8a91ce5bcb6d14b561b85c80fd2c20fb0bdb75db
LeaBani <leabani92@gmail.com>
2023-02-22 10:31:51 +0100

    Contact: add field Select + Control Inputs with useState

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,69 @@
+import { useState } from 'react';
+
+/* eslint-disable react/jsx-no-bind */
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 import './style.scss';
 
 function Contact() {
+  const [change, setChange] = useState('');
+
+  function handleChange(evt) {
+    setChange(evt.target.value);
+    // console.log(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    // console.log('submit');
+  }
+
   return (
     <div className="Contact">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlInput1"
+          value={change}
+          onChange={handleChange}
+        >
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlSelect"
+          value={change}
+          onChange={handleChange}
+        >
+          <Form.Label>Thématique</Form.Label>
+          <Form.Select aria-label="Exemple par défaut">
+            <option>Choix de la thématique</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlTextarea1"
+          value={change}
+          onChange={handleChange}
+        >
           <Form.Label>Ecris ta remarque ici !</Form.Label>
-          <Form.Control type="text" placeholder="Ecris ta remarque ici !" />
+          <Form.Control as="textarea" rows={5} />
         </Form.Group>
 
         <Button variant="primary" type="submit">
-          Submit
+          Envoi
         </Button>
       </Form>
+
     </div>
   );
 }
