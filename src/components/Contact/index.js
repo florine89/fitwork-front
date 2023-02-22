79798375030652c4ce9925c1cf8537228faf3c7d
LeaBani <leabani92@gmail.com>
2023-02-22 10:17:53 +0100

    Contact: Change component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -10,18 +10,13 @@ function Contact() {
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Ecris ta remarque ici !</Form.Label>
+          <Form.Control type="text" placeholder="Ecris ta remarque ici !" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
