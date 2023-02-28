daf4bcbb06651b7a4bccfdb53a0a5e20df10f4ea
florine89 <ferreiraflorine@gmail.com>
2023-02-28 15:00:37 +0100

    update css

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -4,6 +4,12 @@ import logo from '../../assets/femmebureau.jpg';
 function Home() {
   return (
     <div className="home">
+      <h1 className="home-quote">
+        Un esprit sain, dans un corps sain !
+        <span className="home-small">
+          Pour travailler sainement.
+        </span>
+      </h1>
       <img src={logo} className="home-logo" alt="Logo FitWork" />
     </div>
   );
