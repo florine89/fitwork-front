09f0d14431b35d9d081ff60c6e105bede0b3db1a
LeaBani <leabani92@gmail.com>
2023-03-03 15:48:49 +0100

    Program: add comments

--- src/components/Articles/index.js
+++ src/components/Articles/index.js
@@ -17,33 +17,39 @@ import logo from '../../assets/femmebureau.jpg';
 function Articles() {
   const API_BASE_URL = 'http://barrealexandre-server.eddi.cloud:8080/api';
 
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]); // je récupère les articles dans le state local
+  const userId = useSelector((state) => state.user.id); // je recupère mon user id avec redux
 
-  const { id } = useParams();
-  console.log('id de la catégorie', id);
+  const { id } = useParams(); // je recupère l'id de la catégorie dans la requete http
 
-  const userId = useSelector((state) => state.user.id);
+  const idArticle = 1; // TODO a dynamiser
+  console.log(idArticle);
 
+  /**
+   * Cette fonction recupère la liste des articles de la catégorie concernée
+   */
   function getArticleByCategorie() {
     axios.get(`${API_BASE_URL}/category/${id}`).then((response) => {
       setArticles(response.data);
     });
   }
 
-  const idArticle = 1;
-  console.log(idArticle);
-
+  /**
+   * A la soumission du formulaire, évite le comportement par défaut
+   */
   function handleSubmit(evt) {
     evt.preventDefault();
     // console.log('submit');
   }
 
+  /**
+   * Cette fonction ajoute au click un article au programme du user connecté
+   */
   function addArticleToProgram() {
-    console.log('click');
-    console.log('userId', userId);
+    // console.log('userId', userId);
 
     axios
-      .post(`${API_BASE_URL}/article/${idArticle}/program`, { // id article
+      .post(`${API_BASE_URL}/article/${idArticle}/program`, {
         user_id: userId,
       })
       .then((response) => {
@@ -51,6 +57,9 @@ function Articles() {
       });
   }
 
+  /**
+   * A chaque changement d'id, useEffect rappelle la fonction concernée et met à jour l'affichage
+   */
   useEffect(() => {
     getArticleByCategorie();
   }, [id]);
@@ -60,15 +69,15 @@ function Articles() {
       <h1>Articles disponibles pour la categorie</h1>
       <Form onSubmit={handleSubmit}>
         {articles.map((article) => (
-          <article key={article.id} className="card-article">
+          <article key={article.id} className="Articles-card">
             <Card style={{ width: '18rem', height: '25rem' }}>
               <Card.Img variant="top" src={logo} />
               <Card.Body>
                 <Card.Title>{article.title}</Card.Title>
-                <Card.Text className="card-description">
+                <Card.Text className="Articles-card-description">
                   {article.description}
                 </Card.Text>
-                <Button className="card-button" variant="primary" type="submit" onClick={addArticleToProgram}>Ajouter au programme</Button>
+                <Button className="Articles-card-button" variant="primary" type="submit" onClick={addArticleToProgram}>Ajouter au programme</Button>
               </Card.Body>
             </Card>
           </article>
