e9053b01690b33c66bdfa4cfc00c89a337aaccd4
florine89 <ferreiraflorine@gmail.com>
2023-03-03 12:07:18 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './stores';
 import App from './components/App/App';
-import './index.css';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
