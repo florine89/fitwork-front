38a5afb4ec9c4a00d7e40cfdf1ce0b6b040a2f56
florine89 <ferreiraflorine@gmail.com>
2023-03-07 12:05:09 +0100

    delete program ok

--- src/components/Program/index.js
+++ src/components/Program/index.js
@@ -27,7 +27,8 @@ function Program() {
     })
       .then((response) => {
         console.log(response.data);
-        //     setArticles(response.data);
+        const newArticles = articles.filter((article) => article.program_id !== idProgram);
+        setArticles(newArticles);
       });
   }
 
